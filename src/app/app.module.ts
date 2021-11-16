36a9bb71db8d2f661cca4ebeda46a458985c5129
Niraj Sonje <nirajs0607@gmail.com>
2021-11-16 12:14:22 +0530

    angular-jwt-login

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from "@angular/common/http"
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    LoginComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/services/login.service.ts
@@ -0,0 +1,45 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  url = "http://localhost:3600"
+  constructor(private http:HttpClient) { }
+
+  generateToken(credentials:any){
+    return this.http.post(`${this.url}/token`,credentials)
+  }
+  
+
+  //for login
+  loginUser(token:any)
+  {
+    localStorage.setItem("token", token)
+    return true;
+  }
+
+  isLoggedIn()
+  {
+    let token = localStorage.getItem("token");
+    if (token == undefined || token == '' || token == null)
+    {
+      return false;
+    }else{
+      return true;
+    }
+  }
+
+  logout()
+  {
+    localStorage.removeItem('token');
+    return true;
+  }
+
+  getToken()
+  {
+    return localStorage.getItem("token");
+  }
+}
